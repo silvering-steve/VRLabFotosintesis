3b58e82e9fe99cb1fdcfad7745f78a497de15ea7
sstevenadi <sstevenadi@gmail.com>
2021-10-09 23:02:54 +0700

    Add 2 halaman + script animasi awal

---
+++ Assets/Scripts/ChangeHalaman.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeHalaman : MonoBehaviour
+{
+    public GameObject next;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangePanel()
+    {
+        next.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
