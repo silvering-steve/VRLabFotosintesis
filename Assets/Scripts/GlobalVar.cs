7fe132d1207ce943f6ae4b7ff42a6151109f4e94
Aditya Putra <adtptra@gmail.com>
2021-10-10 13:37:40 +0700

    Add script setting  cahaya

--- Assets/Scripts/GlobalVar.cs
+++ Assets/Scripts/GlobalVar.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalVar : MonoBehaviour
+public class GlobalVar
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum CaseEksperimen
     {
-        
+        EKSPERIMEN_LIGHT,
+        EKSPERIMEN_NAHCO3
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// setting untuk mengecek, apakah dia eksperimen dengan cahaya, atau dengan nahco3
+    /// </summary>
+    public static CaseEksperimen caseEksperimen = CaseEksperimen.EKSPERIMEN_LIGHT;
+
+
+    //======================SETTING PERCOBAAN CAHAYA ============================//
+    public enum TingkatCahaya
     {
-        
+        GELAP,
+        SANGAT_REDUP,
+        REDUP,
+        TERANG,
+        SANGAT_TERANG
     }
+    public static TingkatCahaya tingkatCahaya = TingkatCahaya.GELAP;
+    //============================================================================
+
+
+    //======================SETTING PERCOBAAN NAHCO3 ============================//
+    public static int jumlahNahco3;
+    //============================================================================
+
 }
--- Assets/Scripts/PageEksperimenLight.cs
+++ Assets/Scripts/PageEksperimenLight.cs
@@ -1,18 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PageEksperimenLight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private ParticleSystem bubbleParticles;
+
+    [SerializeField]
+    private Image imgCahaya;
+
+    [SerializeField]
+    private ParticleSystem prefab_particle;
+
+    [SerializeField]
+    private Transform transform_parent_gelas;
+
+    [SerializeField]
+    private Slider slider;
+
+    public void ChangeValueSlider(float value)
     {
-        
+        if(value >= 0 && value < 0.2)
+        {
+            //gelap
+            GlobalVar.tingkatCahaya = GlobalVar.TingkatCahaya.GELAP;
+            var a = bubbleParticles.emission;
+            a.rateOverTime = 0;
+        }
+        else if(value >= 0.2 && value < 0.4)
+        {
+            //sangat redup
+            GlobalVar.tingkatCahaya = GlobalVar.TingkatCahaya.SANGAT_REDUP;
+            var a = bubbleParticles.emission;
+            a.rateOverTime = 2;
+        }
+        else if(value >= 0.4 && value < 0.6)
+        {
+            //redup
+            GlobalVar.tingkatCahaya = GlobalVar.TingkatCahaya.REDUP;
+            var a = bubbleParticles.emission;
+            a.rateOverTime = 4;
+        }
+        else if(value >= 0.6 && value < 0.8)
+        {
+            //terang
+            GlobalVar.tingkatCahaya = GlobalVar.TingkatCahaya.TERANG;
+            var a = bubbleParticles.emission;
+            a.rateOverTime = 6;
+        }
+        else
+        {
+            //sangat terang
+            GlobalVar.tingkatCahaya = GlobalVar.TingkatCahaya.SANGAT_TERANG;
+            var a = bubbleParticles.emission;
+            a.rateOverTime = 8;
+        }
+
+        float maxValueOpacity = 0.6f;
+        float opacity = maxValueOpacity * value;
+        imgCahaya.color = new Color(1,1,0,opacity);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnEnable()
     {
-        
+        ResetData();
+    }
+
+    private void OnDisable()
+    {
+        Destroy(bubbleParticles.gameObject);
+    }
+
+
+    /// <summary>
+    /// fungsi yang digunakan untuk mereset data, apabila masuk lagi ke halaman eksperimen light
+    /// </summary>
+    private void ResetData()
+    {
+        //instantiate bubble
+        GameObject bbl = Instantiate(prefab_particle.gameObject, transform_parent_gelas.position, Quaternion.identity);
+        bbl.SetActive(true);
+        bubbleParticles = bbl.GetComponent<ParticleSystem>();
+
+
+        GlobalVar.tingkatCahaya = GlobalVar.TingkatCahaya.GELAP;
+
+        slider.value = 0f;
+
+        ChangeValueSlider(slider.value);
     }
 }
