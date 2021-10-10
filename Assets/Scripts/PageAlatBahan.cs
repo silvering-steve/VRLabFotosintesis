0744896ecfe89a3afd1e30e5c8f4c4d9ca9a3c35
Aditya Putra <33738081+akari23@users.noreply.github.com>
2021-10-10 11:42:03 +0700

    tes

---
+++ Assets/Scripts/PageAlatBahan.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageAlatBahan : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject obj_gelasKimia;
+
+    [SerializeField]
+    public GameObject kawat_pengait;
+
+    [SerializeField]
+    public GameObject corong_kaca;
+
+    [SerializeField]
+    public GameObject tabung_reaksi;
+
+    [SerializeField]
+    public GameObject obj_air;
+
+    [SerializeField]
+    public GameObject obj_kimia;
+
+    [SerializeField]
+    public GameObject tanaman;
+
+    [SerializeField]
+    public GameObject obj_meja;
+
+
+    [Header("LOKASI POSISI")]
+    [SerializeField]
+    private Transform lokasi_gelas;
+    bool startGlass;
+    private void Update()
+    {
+        if (startGlass)
+            obj_gelasKimia.transform.position = Vector2.MoveTowards(obj_gelasKimia.transform.position, lokasi_gelas.transform.position, 10 * Time.deltaTime);
+    }
+
+    public void ClickGelas()
+    {
+        obj_gelasKimia.transform.parent = obj_meja.transform;
+        startGlass = true;
+    }
+
+
+
+}
---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject page_op1;
+    [SerializeField]
+    private GameObject page_op2;
+
+    [SerializeField]
+    public GameObject page_mainMenu;
+
+    private void Start()
+    {
+        StartCoroutine(ie_op1());
+    }
+
+
+    IEnumerator ie_op1()
+    {
+        yield return new WaitForSeconds(4f);
+        StartCoroutine(ie_op2());
+        page_op1.SetActive(false);
+
+    }
+
+    IEnumerator ie_op2()
+    {
+        page_op2.SetActive(true);
+        yield return new WaitForSeconds(4f);
+        page_op2.SetActive(false);
+        page_mainMenu.SetActive(true);
+
+    }
+
+}
