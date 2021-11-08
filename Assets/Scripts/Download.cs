dc74611f4a2e9a0cabaedcac8de24610301c3dcc
sstevenadi <sstevenadi@gmail.com>
2021-11-08 21:35:42 +0700

    up download ma quiz

---
+++ Assets/Scripts/Download.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Download : MonoBehaviour
+{
+    public void Unduh()
+    {
+        Application.OpenURL("https://drive.google.com/uc?export=download&id=1T2HCTVelMFINZuag4IGwGjJN-0TLAi4y");
+    }
+}
